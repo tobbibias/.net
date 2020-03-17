d1e3a73c7339202435e7e2abfa6e49024cf6e7b6
tobbibias <43636107+tobbibias@users.noreply.github.com>
2020-03-17 16:33:51 +0100

    trimmed fat, added class note

---
+++ project/Notable/Notable.Model/Note.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notable.Model
+{
+    class Note : IComparable<Note>
+    {
+
+        public String NoteID { get; set; }
+        public String Content { get; set; }
+        public String Description { get; set; }
+        public String Title { get; set; }
+        public List<String> Tags  { get; set; }
+        public String Course  { get; set; }
+        public DateTime Date { get; set; }
+
+        public int CompareTo(Note other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
